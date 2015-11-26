f0f1dd12d05bcf56585f864d88096b60002717b1
djun <dujun_dj@126.com>
2015-11-26 17:15:06 +0800

    分页   version 0.1

--- WebContent/WEB-INF/js/bootstrap-3.3.5/myPage.js
+++ WebContent/WEB-INF/js/bootstrap-3.3.5/myPage.js
@@ -6,7 +6,7 @@
 			
 			//定义分页结构体
 		    var $pageInfo={
-		    		url : $this.context.baseURI,
+		    		url : $("form[name='searchForm']").attr("action"),//$this.context.baseURI,
 		    		totalCount : $(this).attr("totalCount")*1,//总条数
 		    		currentPage : $(this).attr("currentPage")*1, // 当前页码
 		    		PageSize : $(this).attr('numPerPage')*1,//每页条数
@@ -17,8 +17,10 @@
 		    
 		    //定义分页样式结构体
 		    var $pageStyle={
-		    		styleLaquo_ : 'laquo',//上一页样式
-					styleRaquo_ : 'raquo',//下一页样式
+		    		styleLaquo_ : 'laquo',//首页样式
+					styleRaquo_ : 'raquo',//尾页样式
+					styleLt_ : 'lt',//上一页样式
+					styleGt_ : 'gt'//下一页样式
 		    }
 		    
 		    //初始起始页数、结束页数
@@ -35,34 +37,45 @@
 		    }
 		    
 		    if($pageInfo.currentPage == 0 || $pageInfo.currentPage == 1){
-		    	$pageStyle.styleLaquo_='disabled';
+		    	$pageStyle.styleLaquo_='disabled';//首页样式
+		    	$pageStyle.styleLt_ = 'disabled';//上一页样式
 		    }else if($pageInfo.currentPage == $pageInfo.pageCount){
-		    	$pageStyle.styleRaquo_='disabled';
+		    	$pageStyle.styleRaquo_='disabled';//尾页样式
+		    	$pageStyle.styleGt_ = 'disabled'//下一页样式
 		    }
 		    
 		    
 		    var $html=[];
 		    $html.push('<ul class="pagination">');
 		    $html.push('<li class="'+$pageStyle.styleLaquo_+'"><a href="javascript:void(0)">&laquo;</a></li>');
+		    $html.push('<li class="'+$pageStyle.styleLt_+'"><a href="javascript:void(0)">‹</a></li>');
 		    for(var i = start ; i <= end ; i ++){
 		    	styleButton_ = i == $pageInfo.currentPage || ($pageInfo.currentPage == 0 && i <=1)? 'active' : 'nomal' ;
 		    	$html.push('<li class="'+styleButton_+'"><a href="javascript:void(0)">'+i+'</a></li>');
 		    }
+		    $html.push('<li class="'+$pageStyle.styleGt_+'"><a href="javascript:void(0)">›</a></li>');
 		    $html.push('<li class="'+$pageStyle.styleRaquo_+'"><a href="javascript:void(0)">&raquo;</a></li>');
 		    $html.push('</ul>');
 			
 		    $this.html($html.join(""));
 		    
 		    //绑定数据处理函数
-		    $this.find("ul li[class=\'laquo\'] a").bind("click",function(){
+		    $this.find("ul li[class=\'laquo\'] a").bind("click",function(){//首页
+		    	alert('up');
+		    	redirectTo(1);
+		    });
+		    $this.find("ul li[class=\'lt\'] a").bind("click",function(){//上一页
 		    	alert('up');
 		    	redirectTo($pageInfo.currentPage-1);
 		    });
 		    $this.find("ul li[class=\'nomal\'] a").bind("click",function(){
-//		                  redirectTo(pageinfo.currentPage-1);
 		    	redirectTo($(this).html());
 		    });
-		    $this.find("ul li[class=\'raquo\'] a").bind("click",function(){
+		    $this.find("ul li[class=\'gt\'] a").bind("click",function(){//下一页
+		    	alert('xia');
+		    	redirectTo($pageInfo.currentPage+1);
+		    });
+		    $this.find("ul li[class=\'raquo\'] a").bind("click",function(){//尾页
 		    	alert('xia');
 		    	redirectTo($pageInfo.currentPage==0 ? $pageInfo.currentPage+2 : $pageInfo.currentPage+1);
 		    });
